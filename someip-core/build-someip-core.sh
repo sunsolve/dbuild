8d6e5675e3c37c88538d492a0fb20f323860bcfe
sunsolve <sunsolve@aaa.com>
2018-12-20 12:46:44 +0800

    add someip-core

---
+++ someip-core/build-someip-core.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+cd vsomeip
+git checkout 2.10.21
+rm -rf build && mkdir -p build && cd build
+cmake -D ENABLE_SIGNAL_HANDLING=1 -D CMAKE_INSTALL_PREFIX=/usr ..
+make && make install
+cd .. && rm -rf build
+cd ..
+
+cd capicxx-core-runtime 
+git checkout 3.1.12.4
+rm -rf build && mkdir -p build && cd build
+cmake -D CMAKE_INSTALL_PREFIX=/usr ..
+make && make install
+cd ../..
+
+cd capicxx-someip-runtime 
+git checkout 3.1.12.12
+rm -rf build && mkdir -p build && cd build
+cmake -D USE_INSTALLED_COMMONAPI=ON -D CMAKE_INSTALL_PREFIX=/usr ..
+make && make install
+cd .. && rm -rf build
+cd ..
