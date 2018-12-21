34aa547eaffac5dfcd3e523c0bf689db978f8116
sunsolve <sunsolve@aaa.com>
2018-12-21 19:49:29 +0800

    do not delete build directory after build

--- someip-core/build-someip-core.sh
+++ someip-core/build-someip-core.sh
@@ -6,8 +6,7 @@ git checkout 2.10.21
 rm -rf build && mkdir -p build && cd build
 cmake -D ENABLE_SIGNAL_HANDLING=1 -D CMAKE_INSTALL_PREFIX=/usr ..
 make && make install
-cd .. && rm -rf build
-cd ..
+cd ../..
 
 cd capicxx-core-runtime 
 git checkout 3.1.12.4
@@ -21,5 +20,4 @@ git checkout 3.1.12.12
 rm -rf build && mkdir -p build && cd build
 cmake -D USE_INSTALLED_COMMONAPI=ON -D CMAKE_INSTALL_PREFIX=/usr ..
 make && make install
-cd .. && rm -rf build
-cd ..
+cd ../..
