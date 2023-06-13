fe51df3f348582f9a4ede377ab3606157fde5887
Z0w0 <weizuo@gmail.com>
2023-06-13 19:36:35 +0800

    update someip to version 3.1.20

--- someip-core/build-someip-core.sh
+++ someip-core/build-someip-core.sh
@@ -2,21 +2,21 @@
 
 
 cd vsomeip
-git checkout 2.10.21
+git checkout 3.1.20.3
 rm -rf build && mkdir -p build && cd build
 cmake -D ENABLE_SIGNAL_HANDLING=1 -D CMAKE_INSTALL_PREFIX=/usr ..
 make && make install
 cd ../..
 
 cd capicxx-core-runtime 
-git checkout 3.1.12.4
+git checkout 3.2.0.1
 rm -rf build && mkdir -p build && cd build
 cmake -D CMAKE_INSTALL_PREFIX=/usr ..
 make && make install
 cd ../..
 
 cd capicxx-someip-runtime 
-git checkout 3.1.12.12
+git checkout 3.2.0.1
 rm -rf build && mkdir -p build && cd build
 cmake -D USE_INSTALLED_COMMONAPI=ON -D CMAKE_INSTALL_PREFIX=/usr ..
 make && make install
